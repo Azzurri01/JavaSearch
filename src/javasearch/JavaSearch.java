10291f3a5fb0a60b970887b296ba66e09acec745
Azzurri01 <32220240+Azzurri01@users.noreply.github.com>
2021-04-14 22:05:54 +0200

    Second commit

--- src/javasearch/JavaSearch.java
+++ src/javasearch/JavaSearch.java
@@ -16,10 +16,18 @@ public class JavaSearch {
     
     public static void main(String[] args) 
     {
-        int[] numbers = {10, 4, 6, 8, 7, 1, 0, 3, 9, 2, 5};
+        //Linear search array
+        int[] linearSearchArray = {0,1,2,3,4,5,6,7,8,9,10};   //Linear search performs search from position 0 to the end of the array. Data does not have to be sorted.
+        Search l = new Search();    //Instantiate new object for Linear Search
+        System.out.println("Linear Search results\n----------------------");
+        l.linearSearch(linearSearchArray, 5);   //Execute Linear search method
         
-        Search s = new Search();
-        s.linearSearch(numbers, 4);
+        System.out.println("");
+        
+        //Binary search array
+        int[] binarySearchArray = {0,1,2,3,4,5,6,7,8,9,10};   //Binary search requires data to be sorted
+        Search b = new Search();    //Instantiate new object for Binary Search
+        System.out.println("Binary Search results\n----------------------");
+        b.BinarySearch(binarySearchArray, 5);  //Execute Binary search method
     }
-    
 }
--- src/javasearch/Search.java
+++ src/javasearch/Search.java
@@ -13,6 +13,7 @@ public class Search
     {
         int position=-1;
         boolean found = false;
+        int counter=1;
         
         for(int i=0; i<data.length && found==false; i++)
         {
@@ -24,14 +25,48 @@ public class Search
             }
                 else
                 {
-                    System.out.println("Value " + value + " not found in position:" + data[i] + " of the data array!");
+                    System.out.println("Data not found! Runtime attempt: " + counter);
+                    counter++;
                 }
         }
         return position;
     }
     
-    /*public int BinarySearch()
+    public int BinarySearch(int[] data, int value)
     {
+        //variables
+        int position=-1;
+        int left = 0;
+        int right = data.length-1;
+        boolean found=false;
+        int counter=1;
         
-    }*/
+        //Algorithm
+        while(!found)
+        {
+            int middle =(left+right)/2;
+            
+            if(data[middle]==value)
+            {
+                found=true;
+                position=middle;
+            }
+                else if(data[middle]<value)
+                {
+                    left=middle+1;
+                }
+                    else if(data[middle]>value)
+                    {
+                        right=middle-1;
+                    }
+
+            //Count how many time loop was executed
+            System.out.println("Data not found! Runtime attempt: " + counter);
+            counter++;
+        }
+        
+        //Return array position of value
+        System.out.println("Value: " + value + " found! Location: " + position + ".");
+        return position;
+    }
 }
